09371acafe5a5372abb54f1503dc87833a1d05c0
AnnaJ <annaspaulimp@gmail.com>
2024-06-11 22:26:28 +0530

    successful

---
+++ App.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const { coursemodel } = require("./model/Course")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://Annajimmy:annajimmychirackal@cluster0.moqndmi.mongodb.net/coursedb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post(
+    "/add", (req, res) => {
+        let input = req.body
+        let course = new coursemodel(input)
+        course.save()
+        console.log(course)
+        res.json({"status":"success"})
+    }
+)
+
+app.get(
+    "/search", (req, res) => {
+        res.send("Search done")
+    }
+)
+
+app.get(
+    "/viewall", (req, res) => {
+        coursemodel.find().get(
+            (data)=>{
+                res.json(data)
+            }
+        ).catch(
+            (error)=>{
+                res.json(error)
+            }
+        )
+    }
+)
+
+app.listen(8084, () => {
+    console.log("Server started")
+}
+)
